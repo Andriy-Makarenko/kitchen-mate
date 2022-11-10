9bb6d647dce1f5474bf64829ed65db7b189ce3e3
Andriy Makarenko <andriy@mate.academy>
2022-11-10 14:13:37 +0200

    Add Cook model in models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -8,3 +8,14 @@ class DishType(models.Model):
 
     class Meta:
         ordering = ["name"]
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(max_length=2)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
+
