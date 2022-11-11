fc58ff270390305b4239d25e23761fe7fd5de457
Andriy Makarenko <andriy@mate.academy>
2022-11-11 18:38:16 +0200

    Add default sectet key

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
-# SECRET_KEY = 'django-insecure-s48u7t4)su^uqkahwp!utme!6%pfb(tqp8v8ulv5h&_l8kkql@'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-s48u7t4)su^uqkahwp!utme!6%pfb(tqp8v8ulv5h&_l8kkql@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
