39488a14594602f105321721a27b828df44273d4
Andriy Makarenko <andriy@mate.academy>
2022-11-13 16:30:58 +0200

    Udpadt settings.py & requirements.txt

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -27,10 +27,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 INTERNAL_IPS = [
-    "127.0.0.1",
+    '127.0.0.1',
 ]
 # Application definition
 
